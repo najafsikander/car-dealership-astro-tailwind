2e1921dbd0b8179c35e803e657147acc093d4db1
najafsikander <najafsikander@yahoo.com>
2024-12-26 16:29:53 +0400

    Initialized project and configured tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				bg_color: '#212934',
+				font_color: '#222222',
+				nav_link_color: '#212934',
+				custom_red: '#bd162d',
+				custom_blue: '#2364c4',
+			}
+		},
+	},
+	plugins: [],
+}
